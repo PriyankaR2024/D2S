14b3bc95d532e4f972d84b0baebcc056c94cd8b4
Priyanka <pbangarirawat@gmail.com>
2025-04-05 12:01:55 +0530

    server.js

---
+++ backened/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(body.Parser.urlencoded({extended:true}));
+
+app.get('/' ,(reg,res)=>{
+  res.send('backened is running ');
+});
+
+app.listen(PORT , () =>{
+  console.log("Server is running on http://localhost:${PORT}");
+});
+              
